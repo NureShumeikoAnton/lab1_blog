47493bfbbce004a67e746b828137aa25c3b45842
Zhmykh <rgenbach@gmail.com>
2025-03-31 14:54:24 +0300

    4th task

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
+from rest_framework import viewsets
+from .serializers import ArticleSerializer
+
 
 def articles_list(request):
     articles = Article.objects.all()
@@ -7,6 +10,7 @@ def articles_list(request):
         'articles': articles
     }
     return render(request, 'articles/articles_list.html', context)
+
 
 def articles_by_author(request, author_name):
     articles = Article.objects.filter(author=author_name)
@@ -16,9 +20,15 @@ def articles_by_author(request, author_name):
     }
     return render(request, 'articles/author_articles.html', context)
 
+
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     context = {
         'article': article
     }
     return render(request, 'articles/article_detail.html', context)
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
